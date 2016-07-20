981bf00f0d12e27d37092c99fcb4644969a7bd43
Cesare Rossi <cesare.rossi@terradue.com>
2016-07-20 14:54:59 +0200

    Fixed typos

--- src/main/app-resources/stemp/run.sh
+++ src/main/app-resources/stemp/run.sh
@@ -53,16 +53,17 @@ function main() {
   
   ciop-log "INFO" "Converting Digital Elevation Model to GeoTIFF"
   local dem_identifier=$( basename ${dem} )
-  dem_identifier=${dem_identifier%*.}
+  ls -l ${PROCESSING_HOME}
+  ls -l ${dem}
   
-  local dem_geotiff=$( convertDemToGeoTIFF "${dem}.rsc" "${dem}" "${dem_identifier}" "${PROCESSING_HOME}" )
+  local dem_geotiff=$( convertDemToGeoTIFF "${dem}.rsc" "${dem}" "${dem_identifier%*.}" "${PROCESSING_HOME}" )
   ciop-log "INFO" "------------------------------------------------------------"
   
   ciop-log "INFO" "Croppig Digital Elevation Model"
   
   # Extent in degrees
   local extent=0.2
-  local cropped_dem = $( cropDem "${dem_geotiff}" "${dem_identifier}" "${PROCESSING_HOME}" "${v_lon}" "${v_lat}" "${extent}" )
+  local cropped_dem = $( cropDem "${dem_geotiff}" "${dem_identifier%*.}" "${PROCESSING_HOME}" "${v_lon}" "${v_lat}" "${extent}" )
   ciop-log "INFO" "------------------------------------------------------------"
   
   ciop-log "INFO" "Getting input product" 
@@ -119,7 +120,7 @@ function main() {
   if [ ${v_lat} -le 0 ]; then
     
     ciop-log "INFO" "Converting DEM to UTM Zone S"
-    gdalwarp -t_srs "+proj=utm +zone=${UTM_ZONE} +south +datum=WGS84" ${cropped_dem} ${PROCESSING_HOME}/${dem_identifier}_UTM.TIF 1>&2
+    gdalwarp -t_srs "+proj=utm +zone=${UTM_ZONE} +south +datum=WGS84" ${cropped_dem} ${PROCESSING_HOME}/${dem_identifier%*.}_UTM.TIF 1>&2
     
     if [ "${mission,,}" = "landsat8" ]; then
       ciop-log "INFO" "Setting the proper UTM Zone ${UTM_ZONE} for the B10 TIF"
@@ -137,12 +138,12 @@ function main() {
     
   else
     ciop-log "INFO" "Converting DEM to UTM Zone N"
-    gdalwarp -t_srs "+proj=utm +zone=${UTM_ZONE} +datum=WGS84" ${cropped_dem} ${PROCESSING_HOME}/${dem_identifier}_UTM.TIF 1>&2
+    gdalwarp -t_srs "+proj=utm +zone=${UTM_ZONE} +datum=WGS84" ${cropped_dem} ${PROCESSING_HOME}/${dem_identifier%*.}_UTM.TIF 1>&2
     ciop-log "INFO" "------------------------------------------------------------"
   fi
   
   ciop-log "INFO" "Setting DEM resolution to 90m" 
-  gdalwarp -tr 90 -90 ${PROCESSING_HOME}/${dem_identifier}_UTM.TIF ${PROCESSING_HOME}/${dem_identifier}_UTM_90.TIF 1>&2
+  gdalwarp -tr 90 -90 ${PROCESSING_HOME}/${dem_identifier%*.}_UTM.TIF ${PROCESSING_HOME}/${dem_identifier%*.}_UTM_90.TIF 1>&2
   ciop-log "INFO" "------------------------------------------------------------"
   
   ciop-log "INFO" "Preparing file_input.cfg" 
